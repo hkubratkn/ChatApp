9a6a50a70bce595bdce623bbe91497f6630c4a90
kubratekin <soso@>
2024-05-14 14:21:42 +0300

    0

--- app/src/main/java/com/zepi/social_chat_food/model/service/AccountService.kt
+++ app/src/main/java/com/zepi/social_chat_food/model/service/AccountService.kt
@@ -1,6 +1,6 @@
-package com.zepi.social_chat_food.iraaa.model.service
+package com.zepi.social_chat_food.model.service
 
-import com.zepi.social_chat_food.iraaa.model.UserUid
+import com.zepi.social_chat_food.model.UserUid
 import kotlinx.coroutines.flow.Flow
 
 interface AccountService {
