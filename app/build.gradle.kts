3c1c0d3f46c3cefe4d10ad784eb29601482cb848
kubratekin <soso@>
2024-05-20 12:21:49 +0300

    12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.kapirti.video_food_delivery_shopping"
         minSdk = 21
         targetSdk = 34
-        versionCode = 24
-        versionName = "1.0.24"
+        versionCode = 25
+        versionName = "1.0.25"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
